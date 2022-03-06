7a6602f4a7bf77eb606db51e27648330e043ce02
Dueel <diego.bar@live.it>
2022-03-06 16:57:19 +0100

    DbContext and entities

---
+++ EFCoreTest.API/Models/Customer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreTest.API.Models
+{
+    [Index(nameof(City), Name = "City")]
+    [Index(nameof(CompanyName), Name = "CompanyName")]
+    [Index(nameof(PostalCode), Name = "PostalCode")]
+    [Index(nameof(Region), Name = "Region")]
+    public partial class Customer
+    {
+        public Customer()
+        {
+            Orders = new HashSet<Order>();
+            CustomerTypes = new HashSet<CustomerDemographic>();
+        }
+
+        [Key]
+        [Column("CustomerID")]
+        [StringLength(5)]
+        public string CustomerId { get; set; } = null!;
+        [StringLength(40)]
+        public string CompanyName { get; set; } = null!;
+        [StringLength(30)]
+        public string? ContactName { get; set; }
+        [StringLength(30)]
+        public string? ContactTitle { get; set; }
+        [StringLength(60)]
+        public string? Address { get; set; }
+        [StringLength(15)]
+        public string? City { get; set; }
+        [StringLength(15)]
+        public string? Region { get; set; }
+        [StringLength(10)]
+        public string? PostalCode { get; set; }
+        [StringLength(15)]
+        public string? Country { get; set; }
+        [StringLength(24)]
+        public string? Phone { get; set; }
+        [StringLength(24)]
+        public string? Fax { get; set; }
+
+        [InverseProperty(nameof(Order.Customer))]
+        public virtual ICollection<Order> Orders { get; set; }
+
+        [ForeignKey("CustomerId")]
+        [InverseProperty(nameof(CustomerDemographic.Customers))]
+        public virtual ICollection<CustomerDemographic> CustomerTypes { get; set; }
+    }
+}
---
+++ EFCoreTest.API/NorthwindDBContext.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using EFCoreTest.API.Models;
+
+namespace EFCoreTest.API
+{
+    public partial class NorthwindDBContext : DbContext
+    {
+        public NorthwindDBContext()
+        {
+        }
+
+        public NorthwindDBContext(DbContextOptions<NorthwindDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Category> Categories { get; set; } = null!;
+        public virtual DbSet<Customer> Customers { get; set; } = null!;
+        public virtual DbSet<CustomerDemographic> CustomerDemographics { get; set; } = null!;
+        public virtual DbSet<Employee> Employees { get; set; } = null!;
+        public virtual DbSet<Order> Orders { get; set; } = null!;
+        public virtual DbSet<OrderDetail> OrderDetails { get; set; } = null!;
+        public virtual DbSet<Product> Products { get; set; } = null!;
+        public virtual DbSet<Region> Regions { get; set; } = null!;
+        public virtual DbSet<Shipper> Shippers { get; set; } = null!;
+        public virtual DbSet<Supplier> Suppliers { get; set; } = null!;
+        public virtual DbSet<Territory> Territories { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Name=ConnectionStrings:Northwind");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.Property(e => e.CustomerId).IsFixedLength();
+
+                entity.HasMany(d => d.CustomerTypes)
+                    .WithMany(p => p.Customers)
+                    .UsingEntity<Dictionary<string, object>>(
+                        "CustomerCustomerDemo",
+                        l => l.HasOne<CustomerDemographic>().WithMany().HasForeignKey("CustomerTypeId").OnDelete(DeleteBehavior.ClientSetNull).HasConstraintName("FK_CustomerCustomerDemo"),
+                        r => r.HasOne<Customer>().WithMany().HasForeignKey("CustomerId").OnDelete(DeleteBehavior.ClientSetNull).HasConstraintName("FK_CustomerCustomerDemo_Customers"),
+                        j =>
+                        {
+                            j.HasKey("CustomerId", "CustomerTypeId").IsClustered(false);
+
+                            j.ToTable("CustomerCustomerDemo");
+
+                            j.IndexerProperty<string>("CustomerId").HasMaxLength(5).HasColumnName("CustomerID").IsFixedLength();
+
+                            j.IndexerProperty<string>("CustomerTypeId").HasMaxLength(10).HasColumnName("CustomerTypeID").IsFixedLength();
+                        });
+            });
+
+            modelBuilder.Entity<CustomerDemographic>(entity =>
+            {
+                entity.HasKey(e => e.CustomerTypeId)
+                    .IsClustered(false);
+
+                entity.Property(e => e.CustomerTypeId).IsFixedLength();
+            });
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.HasOne(d => d.ReportsToNavigation)
+                    .WithMany(p => p.InverseReportsToNavigation)
+                    .HasForeignKey(d => d.ReportsTo)
+                    .HasConstraintName("FK_Employees_Employees");
+
+                entity.HasMany(d => d.Territories)
+                    .WithMany(p => p.Employees)
+                    .UsingEntity<Dictionary<string, object>>(
+                        "EmployeeTerritory",
+                        l => l.HasOne<Territory>().WithMany().HasForeignKey("TerritoryId").OnDelete(DeleteBehavior.ClientSetNull).HasConstraintName("FK_EmployeeTerritories_Territories"),
+                        r => r.HasOne<Employee>().WithMany().HasForeignKey("EmployeeId").OnDelete(DeleteBehavior.ClientSetNull).HasConstraintName("FK_EmployeeTerritories_Employees"),
+                        j =>
+                        {
+                            j.HasKey("EmployeeId", "TerritoryId").IsClustered(false);
+
+                            j.ToTable("EmployeeTerritories");
+
+                            j.IndexerProperty<int>("EmployeeId").HasColumnName("EmployeeID");
+
+                            j.IndexerProperty<string>("TerritoryId").HasMaxLength(20).HasColumnName("TerritoryID");
+                        });
+            });
+
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity.Property(e => e.CustomerId).IsFixedLength();
+
+                entity.Property(e => e.Freight).HasDefaultValueSql("((0))");
+
+                entity.HasOne(d => d.Customer)
+                    .WithMany(p => p.Orders)
+                    .HasForeignKey(d => d.CustomerId)
+                    .HasConstraintName("FK_Orders_Customers");
+
+                entity.HasOne(d => d.Employee)
+                    .WithMany(p => p.Orders)
+                    .HasForeignKey(d => d.EmployeeId)
+                    .HasConstraintName("FK_Orders_Employees");
+
+                entity.HasOne(d => d.ShipViaNavigation)
+                    .WithMany(p => p.Orders)
+                    .HasForeignKey(d => d.ShipVia)
+                    .HasConstraintName("FK_Orders_Shippers");
+            });
+
+            modelBuilder.Entity<OrderDetail>(entity =>
+            {
+                entity.HasKey(e => new { e.OrderId, e.ProductId })
+                    .HasName("PK_Order_Details");
+
+                entity.Property(e => e.Quantity).HasDefaultValueSql("((1))");
+
+                entity.HasOne(d => d.Order)
+                    .WithMany(p => p.OrderDetails)
+                    .HasForeignKey(d => d.OrderId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Order_Details_Orders");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.OrderDetails)
+                    .HasForeignKey(d => d.ProductId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Order_Details_Products");
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.ReorderLevel).HasDefaultValueSql("((0))");
+
+                entity.Property(e => e.UnitPrice).HasDefaultValueSql("((0))");
+
+                entity.Property(e => e.UnitsInStock).HasDefaultValueSql("((0))");
+
+                entity.Property(e => e.UnitsOnOrder).HasDefaultValueSql("((0))");
+
+                entity.HasOne(d => d.Category)
+                    .WithMany(p => p.Products)
+                    .HasForeignKey(d => d.CategoryId)
+                    .HasConstraintName("FK_Products_Categories");
+
+                entity.HasOne(d => d.Supplier)
+                    .WithMany(p => p.Products)
+                    .HasForeignKey(d => d.SupplierId)
+                    .HasConstraintName("FK_Products_Suppliers");
+            });
+
+            modelBuilder.Entity<Region>(entity =>
+            {
+                entity.HasKey(e => e.RegionId)
+                    .IsClustered(false);
+
+                entity.Property(e => e.RegionId).ValueGeneratedNever();
+
+                entity.Property(e => e.RegionDescription).IsFixedLength();
+            });
+
+            modelBuilder.Entity<Territory>(entity =>
+            {
+                entity.HasKey(e => e.TerritoryId)
+                    .IsClustered(false);
+
+                entity.Property(e => e.TerritoryDescription).IsFixedLength();
+
+                entity.HasOne(d => d.Region)
+                    .WithMany(p => p.Territories)
+                    .HasForeignKey(d => d.RegionId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Territories_Region");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
