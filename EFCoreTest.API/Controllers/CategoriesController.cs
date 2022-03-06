0cf3a47c3ecca3ecbcfd834cd802d22532ffed1f
Dueel <diego.bar@live.it>
2022-03-06 17:20:07 +0100

    controller test

---
+++ EFCoreTest.API/Controllers/CategoriesController.cs
@@ -0,0 +1,36 @@
+﻿using EFCoreTest.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreTest.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly NorthwindDBContext _context;
+
+        public CategoriesController(NorthwindDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Category>>> GetCategories()
+        {
+            return Ok(await _context.Categories.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Category>> GetCategory(int id)
+        {
+            var category = await _context.Categories.SingleOrDefaultAsync(x => x.CategoryId == id);
+
+            if (category == null)
+                return NotFound();
+
+            return Ok(category);
+        }
+    }
+}
